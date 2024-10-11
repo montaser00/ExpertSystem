d1e572bd6ee113a6d5c06411965d63ba1182986b
Montaser Mohtaseb <montaser.mohtaseb@yahoo.com>
2024-10-11 20:16:51 +0300

    feat: expert system to predict wealthiness

---
+++ ExpertSystem.Wealth/Engines/WealthExpertSystem.cs
@@ -0,0 +1,74 @@
+﻿using chen0040.ExpertSystem;
+
+namespace ExpertSystem.Wealth.Engines;
+
+public class WealthExpertSystem
+{
+    public RuleInferenceEngine getInferenceEngine()
+	{
+		RuleInferenceEngine es = new RuleInferenceEngine();
+
+		var rule = new Rule("UpperClass");
+		rule.AddAntecedent(new IsClause("HighIncomeProfession", "Yes"));
+		rule.setConsequent(new Clause("Class", "UpperClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("UpperClass");
+		rule.AddAntecedent(new IsClause("BusinessOwnership", "Yes"));
+		rule.setConsequent(new Clause("Class", "UpperClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("UpperClass");
+		rule.AddAntecedent(new IsClause("Investment", "Yes"));
+		rule.setConsequent(new IsClause("Class", "UpperClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("UpperMiddleClass");
+		rule.AddAntecedent(new IsClause("HighEducation", "Yes"));
+		rule.AddAntecedent(new IsClause("ManagerialProfession", "Yes"));
+		rule.setConsequent(new IsClause("Class", "UpperMiddleClass"));
+
+		es.AddRule(rule);
+		
+		rule = new Rule("MiddleClass");
+		rule.AddAntecedent(new IsClause("StableJob", "Yes"));
+		rule.setConsequent(new IsClause("Class", "MiddleClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("MiddleClass");
+		rule.AddAntecedent(new IsClause("HighEducation", "Yes"));
+		rule.setConsequent(new IsClause("Class", "MiddleClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("MiddleClass");
+		rule.AddAntecedent(new IsClause("Education", "Yes"));
+		rule.setConsequent(new IsClause("Class", "MiddleClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("LowerClass");
+		rule.AddAntecedent(new IsClause("NormalJob", "Yes"));
+		rule.setConsequent(new IsClause("Class", "LowerClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("WorkingClass");
+		rule.AddAntecedent(new IsClause("LaborJob", "Yes"));
+		rule.setConsequent(new IsClause("Class", "WorkingClass"));
+		es.AddRule(rule);
+		
+		rule = new Rule("WorkingClass");
+		rule.AddAntecedent(new IsClause("Education", "No"));
+		rule.setConsequent(new IsClause("Class", "WorkingClass"));
+		es.AddRule(rule);
+
+		rule = new Rule("Poor");
+		rule.AddAntecedent(new IsClause("UnEmployed", "Yes"));
+		rule.setConsequent(new IsClause("Class", "Poor"));
+		es.AddRule(rule);
+		
+		rule = new Rule("Poor");
+		rule.AddAntecedent(new IsClause("Education", "No"));
+		rule.setConsequent(new IsClause("Class", "Poor"));
+		es.AddRule(rule);
+		return es;
+	}
+}
---
+++ ExpertSystem.Wealth/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+using chen0040.ExpertSystem;
+using ExpertSystem.Wealth.Engines;
+
+var es = new WealthExpertSystem().getInferenceEngine();
+
+es.AddFact(new IsClause("Investment", "Yes"));
+es.AddFact(new IsClause("HighEducation", "Yes"));
+es.AddFact(new IsClause("ManagerialProfession", "Yes"));
+
+Console.WriteLine("before inference");
+Console.WriteLine("{0}", es.Facts);
+Console.WriteLine("");
+
+es.Infer(); //forward chain
+
+Console.WriteLine("after inference");
+Console.WriteLine("{0}", es.Facts);
+Console.WriteLine("");
+
+var es2 = new WealthExpertSystem().getInferenceEngine();
+
+es2.AddFact(new IsClause("Education", "No"));
+es2.AddFact(new IsClause("StableJob", "Yes"));
+
+Console.WriteLine("before inference");
+Console.WriteLine("{0}", es2.Facts);
+Console.WriteLine("");
+
+es2.Infer();
+
+Console.WriteLine("after inference");
+Console.WriteLine("{0}", es2.Facts);
+Console.WriteLine("");
+
+var es3  = new WealthExpertSystem().getInferenceEngine();
+
+es3.AddFact(new IsClause("UnEmployed", "Yes"));
+
+Console.WriteLine("before inference");
+Console.WriteLine("{0}", es3.Facts);
+Console.WriteLine("");
+
+es3.Infer();
+
+Console.WriteLine("after inference");
+Console.WriteLine("{0}", es3.Facts);
+Console.WriteLine("");
